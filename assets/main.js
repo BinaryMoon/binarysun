962a08bd4460de33eb04775e918152f7ac8a889e
BinaryMoon <binarymoon@gmail.com>
2017-03-05 10:04:11 +0000

    make game page filters work

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,49 @@
 // javascript in here
+;(function( $ ) {
+
+	$( 'document' ).ready(
+		function() {
+
+			init_game_categories_filters();
+
+		}
+	);
+
+	var init_game_categories_filters = function() {
+
+		$( '.game-header .filter a' ).on(
+			'click',
+			function( e ) {
+
+				e.preventDefault();
+
+				var $this = $( this );
+				var category = $this.data( 'category' );
+				var games = $( '.games-list .game' );
+
+				$this.parent().find( 'a' ).removeClass( 'selected' );
+				$this.addClass( 'selected' );
+
+				if ( 'all' === category ) {
+					games.show();
+					return;
+				}
+
+				games.each(
+					function() {
+
+						var $this = $( this );
+						var categories = $this.data( 'categories' );
+						$this.hide();
+						if ( categories.indexOf( category ) >= 0 ) {
+							$this.show();
+						}
+					}
+				);
+
+			}
+		);
+
+	}
+
+})( jQuery );
